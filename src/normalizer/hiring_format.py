a4d7f8738be25e913b867560200b28e379cf999c
lemegeton <165199353+TrueLemegeton@users.noreply.github.com>
2026-09-05 15:11:42 +0300

    feat: add hiring format normalizer

--- src/normalizer/hiring_format.py
+++ src/normalizer/hiring_format.py
@@ -1,14 +1,29 @@
-def normalize_hiring_format(hiring_format:str | None) -> str | None:
+import re
+
+def normalize_hiring_format(hiring_format: str | None) -> str | None:
     """Приводит сырой формат оформления к нормализованному виду."""
 
     if hiring_format is None:
         return None
 
-    available_hiring_formats = [
-    'трудовой договор',
-    'договор гпх с самозанятым',
-    'договор гпх с ип',
-    'договор гпх с физлицом',
-    ]
+    hiring_format = hiring_format.lower().strip()
+    hiring_format = re.sub(r'\s+', '', hiring_format)
+
+    available_hiring_formats = {
+        'трудовойдоговор': 'трудовой договор',
+        'ссамозанятым': 'договор гпх с самозанятым',
+        'сип': 'договор гпх с ип',
+        'сфизлицом': 'договор гпх с физлицом',
+    }
+
+    normalized_hiring_format = []
+
+    for key, value in available_hiring_formats.items():
+        if key in hiring_format:
+            normalized_hiring_format.append(value)
+
+    return {'hiring_format': normalized_hiring_format}
+
+
 
     
